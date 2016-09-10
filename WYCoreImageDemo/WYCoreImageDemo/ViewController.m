e4a483e0bd3a0e9dc559d5d482147a7a165f8957
wyman <wyman.lyu@gmail.com>
2016-09-10 11:12:22 +0800

    滤镜代码

---
+++ WYCoreImageDemo/WYCoreImageDemo/ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  WYCoreImageDemo
+//
+//  Created by yunyao on 16/9/10.
+//  Copyright © 2016年 yunyao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *inputImgView;
+@property (weak, nonatomic) IBOutlet UIImageView *outputImgView;
+@property (weak, nonatomic) IBOutlet UITextField *urlTextField;
+@property (weak, nonatomic) IBOutlet UIButton *doneBtn;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)doneClick:(id)sender {
+    
+    
+}
+
+
+
+
+
+
+
+
+
+@end
