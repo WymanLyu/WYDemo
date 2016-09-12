cf391abf0e1bf1cc2351db51f2b7eece63b6da3b
wyman <wyman.lyu@gmail.com>
2016-09-12 11:28:15 +0800

    完善图片转场

--- WYCoreImageDemo/WYCoreImageDemo/TransitionView.m
+++ WYCoreImageDemo/WYCoreImageDemo/TransitionView.m
@@ -26,11 +26,7 @@
         
         // 2.获取图片url
         NSURL *fileUrl1 =[[NSBundle mainBundle] URLForResource:@"1.jpg" withExtension:nil];
-//        [NSURL fileURLWithPath:@"/Users/yunyao/Desktop/1.jpg"];
         NSURL *fileUrl2 = [[NSBundle mainBundle] URLForResource:@"1副本.jpg" withExtension:nil];
-//                           [NSURL fileURLWithPath:@"/Users/yunyao/Desktop/1副本.jpg"]]
-//        [[NSBundle mainBundle] pathForResource:@"1副本.jpg" ofType:nil];
-//        [NSURL fileURLWithPath:@"/Users/yunyao/Desktop/1副本.jpg"];
         
         // 3.core image 的上下文
         _context = [CIContext contextWithOptions:nil];
@@ -44,9 +40,9 @@
         _transitionFilter  = [CIFilter filterWithName: @"CICopyMachineTransition"];
         NSLog(@"%@", _transitionFilter.attributes);
         // 设置
-        [_transitionFilter setDefaults];
-        // 矢量对象
-        _vetor = [CIVector vectorWithX:0 Y:0 Z:frame.size.width W:frame.size.height];
+//        [_transitionFilter setDefaults];
+        // 矢量对象640 × 960
+        _vetor = [CIVector vectorWithX:0 Y:0 Z:640 W:960];
         [_transitionFilter setValue:_vetor forKey:kCIInputExtentKey];
         
         // 7.定时器
@@ -65,7 +61,7 @@
     // 1.设置转场滤镜的进度 (10s为周期)
     CGFloat t = ([NSDate timeIntervalSinceReferenceDate] - _timeInterval)*0.1;
     NSLog(@"---%f", t);
-    if (t >= 1.0) {
+    if (t >= 0.5) {
         [_timer invalidate];
         _timer = nil;
     }
@@ -73,23 +69,36 @@
     // 2.转场滤镜 获取混合图片
     [_transitionFilter setValue:_inputImage forKey:kCIInputImageKey];
     [_transitionFilter setValue:_transionImage forKey:kCIInputTargetImageKey];
+    
+    // 设置属性
+    [_transitionFilter setValue:@(640) forKey:@"inputWidth"];
+    [_transitionFilter setValue:[CIColor colorWithCGColor:[UIColor blueColor].CGColor] forKey:@"inputColor"];
+    
     // 设置进度
     [_transitionFilter setValue:@(t) forKey:kCIInputTimeKey];
     CIImage *outImg = [_transitionFilter valueForKey:kCIOutputImageKey];
     
     // 3.裁剪滤镜 获取转场中的渲染图片
     CIFilter *cropFilter = [CIFilter filterWithName:@"CICrop"];
-//    NSLog(@"%@", cropFilter.attributes);
     [cropFilter setValue:outImg forKey:kCIInputImageKey];
     // 设置裁剪区
-    [cropFilter setValue:_vetor forKey:@"inputRectangle"];
+    CIVector *cropVector = [CIVector vectorWithCGPoint:CGPointMake(20, 30)];
+    cropVector = [cropVector initWithCGRect:CGRectMake(0, 0, 640, 960)];
+    
+    [cropFilter setValue:cropVector forKey:@"inputRectangle"];
     // 获取结果
     CIImage *resultImg = [cropFilter valueForKey:kCIOutputImageKey];
     
     // 4.获取过程的图片渲染
-    CGImageRef result = [_context createCGImage:resultImg fromRect:[resultImg extent]];
+    CGRect oriRect = [resultImg extent];
+    
+//     _vetor = [CIVector vectorWithX:0 Y:0 Z:oriRect.size.width W:oriRect.size.height];
+    
+    CGImageRef result = [_context createCGImage:resultImg fromRect:oriRect];
     if (result) {
+        
         self.layer.contents = (__bridge id _Nullable)(result);
+//        self.layer.contents = (__bridge id _Nullable)(xximg.CGImage);
         CGImageRelease(result);
     }
 
