d6bd3ec9ef59395822ba84206e89bcdcf627e2e7
wyman <wyman.lyu@gmail.com>
2017-05-14 10:10:36 +0800

    coreaudio

---
+++ CoreAudio/5-13CAStreamFormatTester/5-13CAStreamFormatTester/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  5-13CAStreamFormatTester
+//
+//  Created by wyman on 2017/5/13.
+//  Copyright © 2017年 wyman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+        kAudioFormatFlagIsSignedInteger
+        
+        
+        
+        
+    }
+    return 0;
+}
